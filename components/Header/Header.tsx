0909799cffd23278f962821e8100495b22225f40
Reagan Middlebrook <reagankm@gmail.com>
2024-06-26 13:31:05 -0700

    Make columns full height of view port with vertically centered images

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -9,8 +9,8 @@ const Protest = Protest_Revolution({weight: '400', subsets: ['latin']});
 export default function Header() {
     return (
         <Container size='xs' p='xs'>
-            <Title visibleFrom='md' order={2}>Reagan <Title component='span' inline style={{fontFamily: Protest.style.fontFamily, fontSize:'2rem'}}>&</Title> Kevin</Title>
-            <Title hiddenFrom='md' order={3}>Reagan <Title component='span' inline style={{fontFamily: Protest.style.fontFamily, fontSize:'1.16rem'}}>&</Title> Kevin</Title>
+            <Title visibleFrom='md' order={3}>Reagan <Title component='span' style={{fontFamily: Protest.style.fontFamily, fontSize:'2rem'}}>&</Title> Kevin</Title>
+            <Title hiddenFrom='md' order={3}>Reagan <Title component='span' style={{fontFamily: Protest.style.fontFamily, fontSize:'1.75rem'}}>&</Title> Kevin</Title>
             <Navbar />
         </Container>
     );
--- components/PageWrapper/PageWrapper.tsx
+++ components/PageWrapper/PageWrapper.tsx
@@ -1,9 +1,11 @@
-import { Container, Flex, Grid, Space } from '@mantine/core';
+import { Container, Flex, Grid, Space, Center, Stack } from '@mantine/core';
 import Image from 'next/image';
 import React, { PropsWithChildren } from 'react';
 
 import vineSvg from '../../assets/vine_thing.svg';
 import Header from '../Header/Header';
+
+import styles from './PageWrapper.module.css';
 
 // const Josefin = Josefin_Sans({ weight: '400', subsets: ['latin'] });
 // const Julius = Julius_Sans_One({ weight: '400', subsets: ['latin'] });
@@ -11,28 +13,34 @@ import Header from '../Header/Header';
 
 const PageWrapper = (props: PropsWithChildren) => {
     return (
-        <Grid>
-            <Grid.Col span={{base: 2, sm: 3, lg: 2}}>
-                <Flex visibleFrom='md' h='100%' direction='col' justify='center' align='center'>
+        <Stack align='stretch' justify='center'>
+        <Grid className={styles.mainRoot}>
+            <Grid.Col
+                span={{base: 2, lg: 2, sm: 3}}>
+                <Flex style={{ height: '100vh' }} visibleFrom='md'  direction='col' justify='center' align='center'>
                     <Image className={'bg-opacity-75'} src={vineSvg} />
                 </Flex>
-                <Flex hiddenFrom='md'  h='100%' direction='col' justify='center' align='center'>
+                <Flex style={{ height: '100vh' }} hiddenFrom='md'  h='100%' direction='col' justify='center' align='center'>
                     <Image className={'bg-opacity-25'} src={vineSvg} />
                 </Flex>
             </Grid.Col>
-            <Grid.Col span={{base: 8, sm: 9, lg: 8}}>
-                <Container>
+            <Grid.Col span={{base: 8, lg: 8, sm: 9}}
+                      className={styles.mainCol}
+            >
+                <Stack align='stretch' justify='center'
+                >
                     <Header />
                     <Space h="md" />
                     {props.children}
-                </Container>
+                </Stack>
             </Grid.Col>
             <Grid.Col visibleFrom='md' span={{base: 2}}>
-                <Flex visibleFrom='md' h='100%' direction='col' justify='center' align='center'>
+                <Flex style={{ height: '100vh' }} visibleFrom='md' h='100%' direction='col' justify='center' align='center'>
                     <Image className={'bg-opacity-75'} src={vineSvg} />
                 </Flex>
             </Grid.Col>
         </Grid>
+        </Stack>
     );
 };
 
