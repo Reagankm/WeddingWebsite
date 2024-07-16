9042055aeca39677be439abb5ceda9769ee3f404
Reagan Middlebrook <reagankm@gmail.com>
2024-07-16 09:06:32 -0700

    Revised layout to emphasize dressing up

--- pages/attire.tsx
+++ pages/attire.tsx
@@ -12,8 +12,23 @@ export default function Attire() {
             <Title style={{fontFamily: protest.style.fontFamily}} order={4}>(But really, you can wear<br />whatever you<br />want)</Title>
             <Space h='sm' />
 
-            <Text>We want you comfortable and happy so wear
-                whatever you're going to feel good in.  Some things that are acceptable to wear to our wedding:
+            <Text>For those who want to participate, please dress up. Here are some ideas
+                to get you started.</Text>
+
+            <Link href={'https://www.pinterest.com/middlebrookward/apocalyptic-clothing/'}>
+                <Title style={{fontFamily: protest.style.fontFamily}} order={4}>Apocalyptic Clothing Pictures</Title>
+            </Link>
+
+
+            <Text><b>Note</b> that there will probably be at least a couple children
+                attending, so make sure it's something you'd be comfortable
+                wearing around them. </Text>
+
+            <Text>If dressing up isn't for you, that's fine too.
+                Anything that makes you feel comfortable and happy is okay to wear.
+                For out-of-towners who don't know, Seattle tends to be pretty casual.
+                Here are examples of
+                things that are acceptable to wear to our wedding:
                 </Text>
             <ul>
                 <li>Jeans</li>
@@ -28,18 +43,7 @@ export default function Attire() {
             </ul>  It's all fine
                 by us.  We're just happy to have you.
 
-            <Text>For those who want to participate, please dress up. Apocalyptic
-                clothing can really be anything. Here are some ideas
-                to get you started.</Text>
 
-            <Link href={'https://www.pinterest.com/middlebrookward/apocalyptic-clothing/'}>
-                <Title style={{fontFamily: protest.style.fontFamily}} order={4}>Apocalyptic Clothing Pictures</Title>
-            </Link>
-
-
-            <Text><b>Note</b> that there will probably be at least a couple children
-                attending, so make sure it's something you'd be comfortable
-                wearing around them. </Text>
 
         </PageWrapper>
     )
