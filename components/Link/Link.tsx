61d40c2d1f40c352b031b53e0ce96bd68de58e7e
Reagan Middlebrook <reagankm@gmail.com>
2024-08-20 12:09:55 -0700

    Make link classes optional

--- components/Link/Link.tsx
+++ components/Link/Link.tsx
@@ -2,7 +2,7 @@ import NextJSLink from 'next/link';
 import { ReactNode } from 'react';
 
 export default function Link({ href, className, children }:
-{ href: string; className: string; children: ReactNode }) {
+{ href: string; className?: string; children: ReactNode }) {
 
     return <NextJSLink target='_blank' className={className} href={href}>
         {children}
