56cb652ce7331e35357073dd0f704af8e4211876
Reagan Middlebrook <reagankm@gmail.com>
2024-09-28 17:21:09 -0700

    Make Header heading a link to home

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,16 +1,22 @@
 import { Container, Title } from '@mantine/core';
 import { Protest_Revolution } from 'next/font/google';
+import Link from 'next/link';
 import React from 'react';
 
 import Navbar from '../Navbar';
+import styles from './Header.module.css';
 
 const Protest = Protest_Revolution({weight: '400', subsets: ['latin']});
 
 export default function Header() {
     return (
         <Container fluid size='xs' p='xs'>
+            <Link className={styles.titleLink} href={'/'}>
             <Title visibleFrom='md' order={3}>Reagan <Title component='span' style={{fontFamily: Protest.style.fontFamily, fontSize:'2rem'}}>&</Title> Kevin</Title>
+            </Link>
+            <Link className={styles.titleLink} href={'/'}>
             <Title hiddenFrom='md' order={3}>Reagan <Title component='span' style={{fontFamily: Protest.style.fontFamily, fontSize:'1.75rem'}}>&</Title> Kevin</Title>
+            </Link>
             <Navbar />
         </Container>
     );
