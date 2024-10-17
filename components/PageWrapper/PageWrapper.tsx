17517c1314903a49dac4c1f39c44e5cde5fd5aed
Reagan Middlebrook <reagankm@gmail.com>
2024-10-17 15:31:02 -0700

    Add footer with spacing and copyright

--- components/PageWrapper/PageWrapper.tsx
+++ components/PageWrapper/PageWrapper.tsx
@@ -1,4 +1,4 @@
-import { Flex, Grid, Space, Stack } from '@mantine/core';
+import { Center, Flex, Grid, Space, Stack } from '@mantine/core';
 import Image from 'next/image';
 import React, { PropsWithChildren } from 'react';
 
@@ -11,6 +11,8 @@ import styles from './PageWrapper.module.css';
 // const titleFont = Julius;
 
 const PageWrapper = (props: PropsWithChildren) => {
+    const currentYear = (new Date().getFullYear());
+    const yearTxt = currentYear === 2024 ? "2024" : "2024 - " + currentYear;
     return (
         <Stack align='stretch' justify='center'>
             <Grid className={styles.mainRoot}>
@@ -30,6 +32,41 @@ const PageWrapper = (props: PropsWithChildren) => {
                         <Header />
                         <Space h="md" />
                         {props.children}
+                        <Space h='xs' />
+                        <Center>
+                            © { yearTxt } - Reagan Middlebrook
+                            {/*<p>*/}
+                            {/*    This website by Reagan Middlebrook is licensed under{' '}*/}
+                            {/*    <a*/}
+                            {/*        href="https://creativecommons.org/licenses/by-sa/4.0/?ref=chooser-v1"*/}
+                            {/*        target="_blank"*/}
+                            {/*        rel="license noopener noreferrer"*/}
+                            {/*        style={{ display: "inline-block" }}*/}
+                            {/*    >*/}
+                            {/*        CC BY-SA 4.0*/}
+                            {/*        <img*/}
+                            {/*            src="https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1"*/}
+                            {/*            alt="CC icon"*/}
+                            {/*            style={{ height: "22px", marginLeft: "3px", verticalAlign: "text-bottom" }}*/}
+                            {/*        />*/}
+                            {/*        <img*/}
+                            {/*            src="https://mirrors.creativecommons.org/presskit/icons/by.svg?ref=chooser-v1"*/}
+                            {/*            alt="BY icon"*/}
+                            {/*            style={{ height: "22px", marginLeft: "3px", verticalAlign: "text-bottom" }}*/}
+                            {/*        />*/}
+                            {/*        <img*/}
+                            {/*            src="https://mirrors.creativecommons.org/presskit/icons/sa.svg?ref=chooser-v1"*/}
+                            {/*            alt="SA icon"*/}
+                            {/*            style={{ height: "22px", marginLeft: "3px", verticalAlign: "text-bottom" }}*/}
+                            {/*        />*/}
+                            {/*    </a>*/}
+                            {/*    <p>*/}
+                            {/*        <strong>Photo Disclaimer:</strong> The photographs displayed on this site are not owned by me and are used with permission or in accordance with applicable copyright law. These photos may not be copied, distributed, modified, or reused without the explicit permission of the original copyright holder.*/}
+                            {/*    </p>*/}
+                            {/*</p>*/}
+
+                        </Center>
+                        <Space h='xs' />
                     </Stack>
                 </Grid.Col>
                 <Grid.Col visibleFrom='md' span={{base: 2}}>
@@ -37,8 +74,11 @@ const PageWrapper = (props: PropsWithChildren) => {
                           justify='center' align='center'>
                         <Image priority alt='vine illustration' className={'bg-opacity-75'} src={vineSvg} />
                     </Flex>
+
                 </Grid.Col>
+
             </Grid>
+
         </Stack>
     );
 };
