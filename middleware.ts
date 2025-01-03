694b5cc06f48af1c69253a57d17d4e3c92eadfa7
Reagan Middlebrook <reagankm@gmail.com>
2025-01-03 14:00:24 -0800

    Add types to middleware

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
-export function middleware(request) {
+export function middleware(request: NextRequest) {
   const url = request.nextUrl.clone();
 
   // Normalize path to lowercase
